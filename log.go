7610ead6f0339068412896b0396327c1243c8194
PauSabatesC <camposabates@gmail.com>
2022-05-30 17:13:36 +0200

    Bugfix: colored space

--- log.go
+++ log.go
@@ -9,7 +9,8 @@ import (
 func Info(a ...any) {
 	c := color.New(color.FgCyan)
 
-	c.Printf("INFO: ")
+	c.Printf("INFO:")
+	fmt.Printf(" ")
 	fmt.Println(a...)
 }
 
@@ -18,14 +19,16 @@ func BigInfo(a ...any) {
 	c.Add(color.Bold)
 	c.Add(color.BgHiWhite)
 
-	c.Printf("INFO: ")
+	c.Printf("INFO:")
+	fmt.Printf(" ")
 	fmt.Println(a...)
 }
 
 func Warn(a ...any) {
 	c := color.New(color.FgYellow)
 
-	c.Printf("WARNING: ")
+	c.Printf("WARNING:")
+	fmt.Printf(" ")
 	fmt.Println(a...)
 }
 
@@ -34,14 +37,16 @@ func BigWarn(a ...any) {
 	c.Add(color.Bold)
 	c.Add(color.BgHiYellow)
 
-	c.Printf("WARNING: ")
+	c.Printf("WARNING:")
+	fmt.Printf(" ")
 	fmt.Println(a...)
 }
 
 func Error(a ...any) {
 	c := color.New(color.FgRed)
 
-	c.Printf("ERROR: ")
+	c.Printf("ERROR:")
+	fmt.Printf(" ")
 	fmt.Println(a...)
 }
 
@@ -50,6 +55,7 @@ func BigError(a ...any) {
 	c.Add(color.Bold)
 	c.Add(color.BgHiRed)
 
-	c.Printf("ERROR: ")
+	c.Printf("ERROR:")
+	fmt.Printf(" ")
 	fmt.Println(a...)
 }
